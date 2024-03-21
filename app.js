9174aa6c1705f9eb8045da258848d7d7db83b2e9
WendyWang1031 <w5569590@gmail.com>
2024-03-21 12:00:03 +0800

    rename variable name , fix showFinalMessage , fix reset and restart

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ console.log("answer: " + targetNumber);
 
 //  在全域環境宣告userGuessTimes使用者猜測次數為0
 let userGuessTimes = 0;
-let webShowTimes = 5;
+let restTimes = 5;
 //  宣告變數抓取HTML的元素
-const guessField = document.getElementById("guessField");
+const guessInputField = document.getElementById("guessField");
 const guessSubmit = document.getElementById("guessSubmit");
-const message = document.querySelector(".message");
+const hintMessage = document.querySelector(".message");
 const lowerBound = document.querySelector(".lowerBound");
 const upperBound = document.querySelector(".upperBound");
 const finalMessage = document.querySelector(".finalMessage");
@@ -20,34 +20,42 @@ const titleTimes = document.querySelector(".title-times");
 // 為按鈕添加事件監聽器，執行該函式
 guessSubmit.addEventListener("click", checkGuess);
 finalAnswer.addEventListener("click", showFinalMessage);
-playAgain.addEventListener("click", resetGame);
+playAgain.addEventListener("click", reStartGame);
 
-//  showFinalMassage函數是呈現揭曉答案
+// 揭曉答案
 function showFinalMessage() {
+  event.preventDefault();
   finalMessage.textContent = "揭曉答案是：" + targetNumber;
 }
 
-// resetGame 重置遊戲
+// 重置遊戲
 function resetGame() {
   targetNumber = Math.floor(Math.random() * 100) + 1;
   console.log("answer: " + targetNumber);
   userGuessTimes = 0;
-  webShowTimes = 5;
-  message.textContent = "";
-  finalMessage.textContent = "";
+  restTimes = 5;
   guessSubmit.disabled = false;
+  hintMessage.textContent = "";
   lowerBound.textContent = 1;
   upperBound.textContent = 100;
+  finalMessage.textContent = "";
   hintRestartSeconds.textContent = "";
   titleTimes.textContent = 5;
-  guessField.value = "";
-  guessField.focus();
+  guessInputField.value = "";
+  guessInputField.focus();
 }
 
+// 重新開啟遊戲
+function reStartGame() {
+  event.preventDefault();
+  resetGame();
+}
+
+// 確認使用者猜測的數字是否符合條件
 function checkGuess() {
   //使用preventDefault表單默認提交行為會被阻止，頁面不會被重新加載
   event.preventDefault();
-  let userGuess = parseInt(guessField.value);
+  let userGuess = parseInt(guessInputField.value);
   console.log("userGuess: " + userGuess);
   if (
     isNaN(userGuess) ||
@@ -63,59 +71,60 @@ function checkGuess() {
   }
 }
 
-// Guess function
+// 提交猜測數字，頁面有相對應的反饋
 function submitGuess() {
-  let userGuess = parseInt(guessField.value);
-  webShowTimes--;
+  let userGuess = parseInt(guessInputField.value);
+  restTimes--;
   userGuessTimes++;
   console.log("userGuessTimes: " + userGuessTimes);
-  //檢查是否輸入文字
+
   if (userGuess === targetNumber && userGuessTimes <= 5) {
-    message.textContent = `恭喜你，你猜對了！你這次猜了 ${userGuessTimes}  次哦～`;
-    message.style.color = "green";
-    guessSubmit.disabled = true;
+    hintMessage.textContent = `恭喜你，你猜對了！你這次猜了 ${userGuessTimes}  次哦～`;
+    hintMessage.style.color = "green";
+    guessInputField.disabled = true;
     titleTimes.textContent = 0;
     countDownAndReset();
   } else if (userGuess < targetNumber && userGuessTimes < 5) {
-    message.textContent = `太小了，再試一次。你已經猜了${userGuessTimes} 次哦～`;
-    message.style.color = "red";
+    hintMessage.textContent = `太小了，再試一次。你已經猜了${userGuessTimes} 次哦～`;
+    hintMessage.style.color = "red";
     lowerBound.textContent = userGuess;
-    titleTimes.textContent = webShowTimes;
+    titleTimes.textContent = restTimes;
   } else if (userGuess > targetNumber && userGuessTimes < 5) {
-    message.textContent = `太大了，再試一次。你已經猜了${userGuessTimes} 次哦～`;
-    message.style.color = "red";
+    hintMessage.textContent = `太大了，再試一次。你已經猜了${userGuessTimes} 次哦～`;
+    hintMessage.style.color = "red";
     upperBound.textContent = userGuess;
-    titleTimes.textContent = webShowTimes;
+    titleTimes.textContent = restTimes;
   } else {
-    message.textContent = `You Failed! 答案是 ${targetNumber}`;
-    message.style.color = "red";
-    guessSubmit.disabled = true;
+    hintMessage.textContent = `You Failed ! 答案是 ${targetNumber}`;
+    hintMessage.style.color = "red";
+    guessInputField.disabled = true;
     countDownAndReset();
   }
 
-  guessField.value = "";
-  guessField.focus();
+  guessInputField.value = "";
+  //focus元素為設置焦點
+  guessInputField.focus();
 }
 
-//更改瀏覽器的倒數數字文本
+//更改瀏覽器的倒數文本
 function display(result) {
   hintRestartSeconds.textContent = result;
 }
-//倒數計時器去呼叫display的函數
+//倒數計時器放入三個參數，使用IIFE並放入時間計時器函式能被立即執行
 function delayedDisplay(string, seconds, callback) {
   setTimeout(() => {
     callback(string);
   }, seconds);
 }
 
-//讓倒數的函數相對應該有的是值
+//讓delayedDisplay放入參數，製造出倒數與文字效果
 function countDownAndReset() {
-  delayedDisplay("倒數計時5秒即將開啟下一局", 0, display);
-  delayedDisplay("倒數計時4秒即將開啟下一局", 1000, display);
-  delayedDisplay("倒數計時3秒即將開啟下一局", 2000, display);
-  delayedDisplay("倒數計時2秒即將開啟下一局", 3000, display);
-  delayedDisplay("倒數計時1秒即將開啟下一局", 4000, display);
-  delayedDisplay("倒數計時0秒即將開啟下一局", 5000, () => {
+  delayedDisplay("倒數計時5秒，即將開啟下一局", 0, display);
+  delayedDisplay("倒數計時4秒，即將開啟下一局", 1000, display);
+  delayedDisplay("倒數計時3秒，即將開啟下一局", 2000, display);
+  delayedDisplay("倒數計時2秒，即將開啟下一局", 3000, display);
+  delayedDisplay("倒數計時1秒，即將開啟下一局", 4000, display);
+  delayedDisplay("倒數計時0秒，即將開啟下一局", 5000, () => {
     resetGame();
   });
 }
